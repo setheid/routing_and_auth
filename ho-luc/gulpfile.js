a004c1e24670527d4f930938b7663f6be40639be
YOUR FULL NAME <Ho.luc13@gmail.com>
2016-04-29 12:01:55 -0700

    fixed sass bug. now displays blue background

--- ho-luc/gulpfile.js
+++ ho-luc/gulpfile.js
@@ -1,13 +1,25 @@
 'use strict'
 
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 const webpack = require('webpack-stream');
 
 const sources = {
   html: __dirname + '/app/**/*.html',
   js: __dirname + '/app/app.js',
-  test: __dirname + '/test/app_spec.js'
+  test: __dirname + '/test/app_spec.js',
+  css: __dirname +'/app/sass/**/*.scss'
 };
+
+gulp.task('sass', function () {
+  return gulp.src('./app/sass/core.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./app/sass/**/*.scss', ['sass']);
+});
 
 gulp.task('bundle:dev', () => {
   return gulp.src(sources.js)
@@ -26,4 +38,4 @@ gulp.task('bundle:test', () => {
     .pipe(gulp.dest('./test'));
 });
 
-gulp.task('default', ['bundle:dev', 'copy-html']);
+gulp.task('default', ['bundle:dev', 'copy-html', 'sass', 'sass:watch']);
